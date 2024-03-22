0c72077df88767ca705a31b0498233e0e3d32f64
bessaemily <bessaemily24@gmail.com>
2024-03-22 18:08:46 -0300

    ajuste na criação de album
    adicionando inputs para a criação de album

--- src/app/models/album.model.ts
+++ src/app/models/album.model.ts
@@ -1,4 +1,14 @@
 export class Album{
     id!: number;
     nome!: string;
+    anoLancamento!: string;
+    descricao!: string;
+    preco!: number;
+    produto!: string;
+    estoque!: number;
+    id_artista!: number;
+    id_genero!: number;
+    id_gravadora!: number;
+    nomeImagem!: string;
+
 }
